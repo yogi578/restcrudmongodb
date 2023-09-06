e64bddc0f66cd395fbfb997c5b6a8b64a71456ef
Yogesh Padekar <yog.padekar07@gmail.com>
2023-09-06 15:14:47 +0530

    enable spring boot apache tompact support

--- src/main/java/com/example/restcrudmndb/RestcrudmndbApplication.java
+++ src/main/java/com/example/restcrudmndb/RestcrudmndbApplication.java
@@ -2,10 +2,17 @@ package com.example.restcrudmndb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestcrudmndbApplication {
+public class RestcrudmndbApplication extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RestcrudmndbApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RestcrudmndbApplication.class, args);
 	}
